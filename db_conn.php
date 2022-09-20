a40d7e115be724c5847dde6ffe7401a061dde839
Nandalal08 <84070053+Nandalal08@users.noreply.github.com>
2022-09-20 14:05:30 +0530

    last commmit for databse

--- db_conn.php
+++ db_conn.php
@@ -1,11 +1,21 @@
 <?php  
 // $server = "containers-us-west-29.railwayasdfasdf
- $sname = "containers-us-west-29.railway.app";
-$uname = "root";
-$password = "UAvnEkYU3gQMmleuntcS";
-$db_name = "railway";
+//  $sname = "containers-us-west-29.railway.app";
+// $uname = "root";
+// $password = "UAvnEkYU3gQMmleuntcS";
+// $db_name = "railway";
 
-$conn = mysqli_connect($sname, $uname, $password, $db_name);
+// $conn = mysqli_connect($sname, $uname, $password, $db_name);
+
+
+
+$MYSQLDATABASE="railway";
+$MYSQLHOST=    "containers-us-west-80.railway.app";
+$MYSQLPASSWORD="IydtOL4J7OXSWWHwDfVT";
+$MYSQLPORT=    "7124";
+$MYSQLUSER=    "root";
+$MYSQL_URL=    "mysql";
+$conn = mysqli_connect($MYSQLDATABASE, $MYSQLHOST, $MYSQLPASSWORD,$MYSQLPORT,$MYSQLUSER,$MYSQL_URL);
 
 if (!$conn) {
 	echo "Connection failed!";
